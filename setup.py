5711233c83423d45954f81810ce492e8bf7c2837
txq92 <56432960+txq92@users.noreply.github.com>
2021-05-15 23:11:32 +0700

    Add files via upload
    first

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="vnhass",
+    version="1.0.1",
+    author="@trumxuquang",
+    author_email="trumxuquang@gmail.com",
+    description="https://vnhass.blogspot.com/",
+    long_description="https://vnhass.blogspot.com/",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
---
+++ vnhass/app.py
@@ -0,0 +1,8 @@
+# app.py
+def sumerr(a, b):
+    print(f'sai ne {a} - {b}')
+    return a + b +1
+
+def solui(a):
+    return a - 1
+#print(sum(1,7))
